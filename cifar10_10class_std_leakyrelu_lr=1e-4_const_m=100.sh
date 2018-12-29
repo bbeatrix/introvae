dfa9c94a8880e1f155d54b7d944a3295d06dbe27
EC2 Default User <ec2-user@ip-172-31-55-219.ec2.internal>
2018-12-29 11:11:26 +0000

    Added m=100 exp.

---
+++ cifar10_10class_std_leakyrelu_lr=1e-4_const_m=100.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+m=100
+for class in {0..9}
+do
+    CUDA_VISIBLE_DEVICES=0 python3.6 main.py --lr_schedule constant --gcnorm None --seed 0 --model_architecture deepsvdd --oneclass_eval True --normal_class $class --dataset cifar10 --shape 32,32 --train_size 50000 --test_size 10000 --m $m --alpha 0.25 --beta 1.0 --latent_dim 128 --batch_size 200 --lr 0.0001 --nb_epoch 100 --prefix pictures/cifar10_std_leakyrelu_lr=1e-4_constant_m=$m/cifar10_class=$class\_seed=0_m=$m\_alpha=0.25_beta=1.0_dim=128_bs=200_wd=0.000001_std_leakyrelu_lr=1e-4_const --model_path  pictures/cifar10_std_leakyrelu_lr=1e-4_constant/cifar10_class=$class\_seed=0_m=$m\_alpha=0.25_beta=1.0_dim=128_bs=200_wd=0.000001_std_leakyrelu_lr=1e-4_const/model --save_latent True --base_filter_num 32 --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > cifar10_class=$class\_std_leakyrelu_lr=1e-4_const_m=$m.cout 2> cifar10_class=$class\_std_leakyrelu_lr=1e-4_const_m=$m.cerr
+done
