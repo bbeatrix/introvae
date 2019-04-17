4003277b94d58a874d2cbe44978aac7dff2e92b6
csadrian <cs.adrian@gmail.com>
2019-04-17 07:50:57 +0200

    Updated exp file to use params

--- cifar10_vs_svhn_introvae.sh
+++ cifar10_vs_svhn_introvae.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python3.6 main.py --seed 0 --model_architecture dcgan --oneclass_eval True --normal_class -1 --gcnorm None --dataset cifar10 --shape 32,32 --train_size 50000 --test_size 10000 --m 40 --alpha 0.25 --beta 1.0 --latent_dim 128 --batch_size 200 --lr 0.0001 --nb_epoch 200 --prefix pictures/cifar10_vs_svhn_m=40_alpha=0.25 --model_path pictures/cifar10_vs_svhn_m=40_alpha=0.25/model --save_latent True --base_filter_num 16 --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > pictures/cifar10_vs_svhn_m=40_alpha=0.25.cout 2> pictures/cifar10_vs_svhn_m=40_alpha=0.25.cerr
+m=30
+alpha=0.25
+
+CUDA_VISIBLE_DEVICES=0 python3.6 main.py --seed 0 --model_architecture dcgan --oneclass_eval True --normal_class -1 --gcnorm None --dataset cifar10 --shape 32,32 --train_size 50000 --test_size 10000 --m ${m} --alpha ${alpha} --beta 1.0 --latent_dim 128 --batch_size 200 --lr 0.0001 --nb_epoch 200 --prefix pictures/cifar10_vs_svhn_m=40_alpha=0.25 --model_path pictures/cifar10_vs_svhn_m=40_alpha=0.25/model --save_latent True --base_filter_num 16 --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > pictures/cifar10_vs_svhn_m=${m}_alpha=${alpha}.cout 2> pictures/cifar10_vs_svhn_m${m}_alpha=${alpha}.cerr
