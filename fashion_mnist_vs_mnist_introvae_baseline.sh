263a3d9ce9a1b72958be701d17abbfca7b24026b
csadrian <cs.adrian@gmail.com>
2019-04-21 08:12:53 +0200

    Updated exp sh

--- fashion_mnist_vs_mnist_introvae_baseline.sh
+++ fashion_mnist_vs_mnist_introvae_baseline.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-m=30
+#m=30
 alpha=0.0
 base_filter_num=16
 train_dataset='fashion_mnist'
 test_dataset_a='fashion_mnist'
 test_dataset_b='mnist'
-use_augmented_variance_loss=False
-name="${test_dataset_a}_vs_${test_dataset_b}_aug=${use_augmented_variance_loss}_alpha=${alpha}_m=${m}"
-
-CUDA_VISIBLE_DEVICES=1 python main.py --name "fashion_mnist_vs_mnist" --seed 0 --color False --use_augmented_variance_loss ${use_augmented_variance_loss} --test_dataset_b ${test_dataset_b} --test_dataset_a ${test_dataset_a} --model_architecture dcgan_univ --oneclass_eval True --normal_class -1 --gcnorm None --dataset ${train_dataset} --shape 28,28 --train_size 50000 --test_size 10000 --m ${m} --alpha ${alpha} --beta 1.0 --latent_dim 10 --batch_size 50 --lr 0.0001 --nb_epoch 500 --prefix pictures/${name} --model_path pictures/${name}/model --save_latent True --base_filter_num ${base_filter_num} --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > pictures/${name}.cout 2> pictures/${name}.cerr
+#use_augmented_variance_loss=False
+seed=0
+for m in 30 40 50 20 10
+do
+  for use_augmented_variance_loss in False True
+  do
+     name="${test_dataset_a}_vs_${test_dataset_b}_aug=${use_augmented_variance_loss}_alpha=${alpha}_m=${m}_seed=${seed}"
+     CUDA_VISIBLE_DEVICES=0 python main.py --name "fashion_mnist_vs_mnist" --seed ${seed} --color False --use_augmented_variance_loss ${use_augmented_variance_loss} --test_dataset_b ${test_dataset_b} --test_dataset_a ${test_dataset_a} --model_architecture dcgan_univ --oneclass_eval True --normal_class -1 --gcnorm None --dataset ${train_dataset} --shape 28,28 --train_size 50000 --test_size 10000 --m ${m} --alpha ${alpha} --beta 1.0 --latent_dim 10 --batch_size 50 --lr 0.0001 --nb_epoch 500 --prefix pictures/${name} --model_path pictures/${name}/model --save_latent True --base_filter_num ${base_filter_num} --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > pictures/${name}.cout 2> pictures/${name}.cerr
+  done
+done
