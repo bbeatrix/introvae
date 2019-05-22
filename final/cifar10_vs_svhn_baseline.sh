7c58964aea208b3dc37b0ad5ddbdce028b01140c
csadrian <cs.adrian@gmail.com>
2019-05-22 16:04:18 +0200

    asd

---
+++ final/cifar10_vs_svhn_baseline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+m=30
+beta=1.0
+alpha_reconstructed=0.0
+alpha_generated=0.0
+alpha_neg=0.0
+neg_dataset=None
+base_filter_num=32
+train_dataset='cifar10'
+test_dataset_a='cifar10'
+test_dataset_b='svhn_cropped'
+use_augmented_variance_loss=False
+seed=0
+normal_class=-1
+model_architecture='dcgan_univ'
+lr_schedule='constant'
+
+for seed in 1 2 3 4
+do
+   name="FIN_baseline_${test_dataset_a}_vs_${test_dataset_b}_${model_architecture}_alpha_reconstructed=${alpha_reconstructed}_alpha_generated=${alpha_generated}_alpha_neg=${alpha_neg}_neg_dataset=${neg_dataset}_m=${m}_beta=${beta}_nc=${normal_class}_seed=${seed}"
+   CUDA_VISIBLE_DEVICES=1 python main.py --reg_lambda 1.0 --name "FIN_baseline_${test_dataset_a}_vs_${test_dataset_b}_${model_architecture}" --seed ${seed} --color True --use_augmented_variance_loss ${use_augmented_variance_loss} --test_dataset_b ${test_dataset_b} --test_dataset_a ${test_dataset_a} --model_architecture ${model_architecture} --oneclass_eval True --normal_class ${normal_class} --gcnorm None --dataset ${train_dataset} --shape 32,32 --train_size 60000 --test_size 10000 --m ${m} --alpha_reconstructed ${alpha_reconstructed} --alpha_generated ${alpha_generated} --beta ${beta} --latent_dim 10 --batch_size 50 --lr 0.0001 --nb_epoch 100 --prefix pictures/${name} --model_path pictures/${name}/model --save_latent True --base_filter_num ${base_filter_num} --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 1000 --verbose 2 --resnet_wideness 1 --lr_schedule ${lr_schedule} > pictures/${name}.cout 2> pictures/${name}.cerr
+done
