d12edc6b3f3085dab71bf4fdbce3b6f58ae45bf5
csadrian <cs.adrian@gmail.com>
2020-10-07 19:41:07 +0000

    Fix eubo 2

--- main.py
+++ main.py
@@ -318,9 +318,11 @@ def eubo_loss_fn(z, z_mean, z_log_var, reconst_loss):
   
   log_p_z = tf.reduce_sum(log_p_z, axis=-1)
   log_q_z = tf.reduce_sum(log_q_z, axis=-1)
-  # (batch_size x num_samples)
+  # (batch_size, num_samples)
 
-  log_w = tf.reduce_mean(tf.expand_dims(reconst_loss, axis=1) + log_p_z - log_q_z, axis=1)
+  #log_w = tf.reduce_mean(reconst_loss + log_p_z - log_q_z, axis=1)
+  log_w = reconst_loss + log_p_z - log_q_z
+
   w = tf.exp(log_w - tf.reduce_max(log_w, axis=1, keep_dims=True))
   w_hat = w / tf.reduce_sum(w, axis=1, keep_dims=True)
 
