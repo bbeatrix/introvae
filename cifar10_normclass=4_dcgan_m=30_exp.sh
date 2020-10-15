65f27ed6c17bcdae096e4fc5b09872801e2d6645
bbeatrix <bbea.smile@gmail.com>
2020-10-15 17:28:39 +0200

    Remove unused files

--- cifar10_normclass=4_dcgan_m=30_exp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python3.6 main.py --seed 0 --model_architecture dcgan --gcnorm None --oneclass_eval True --normal_class 4 --dataset cifar10 --shape 32,32 --train_size 50000 --test_size 10000 --m 30 --alpha 0.25 --beta 1.0 --latent_dim 128 --batch_size 200 --lr 0.0001 --nb_epoch 100 --prefix pictures/cifar10_normclass=4_dcgan_m=30 --model_path pictures/cifar10_normclass=4_dcgan_m=30/model --save_latent True --base_filter_num 16 --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > cifar10_normclass=4_dcgan_m=30.cout 2> cifar10_normclass=4_dcgan_m=30.cerr
--- cifar10_vs_svhn_introvae.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-m=30
-alpha=0.25
-base_filter_num=32
-name="cifar10_vs_svhn_2xwide"
-CUDA_VISIBLE_DEVICES=0 python3.6 main.py --seed 0 --model_architecture dcgan --oneclass_eval True --normal_class -1 --gcnorm None --dataset cifar10 --shape 32,32 --train_size 50000 --test_size 10000 --m ${m} --alpha ${alpha} --beta 1.0 --latent_dim 128 --batch_size 200 --lr 0.0001 --nb_epoch 200 --prefix pictures/${name}_m=${m}_alpha=${alpha} --model_path pictures/${name}_m=${m}_alpha=${alpha}/model --save_latent True --base_filter_num ${base_filter_num} --encoder_use_bn True --encoder_wd 0.000001 --generator_use_bn True --generator_wd 0.000001 --frequency 100 --verbose 2 --resnet_wideness 1 > pictures/${name}_m=${m}_alpha=${alpha}.cout 2> pictures/${name}_m=${m}_alpha=${alpha}.cerr
