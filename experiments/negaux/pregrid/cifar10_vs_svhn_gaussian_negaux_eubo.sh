9f2b72f37b352df1c6fff6e9edff2413adb5fdbd
bbeatrix <bbea.smile@gmail.com>
2021-01-04 07:40:05 +0000

    Add modified sh files

--- experiments/negaux/pregrid/cifar10_vs_svhn_gaussian_negaux_eubo.sh
+++ experiments/negaux/pregrid/cifar10_vs_svhn_gaussian_negaux_eubo.sh
@@ -26,7 +26,7 @@ gcnorm=None
 save_latent=True
 frequency=1000
 d=$(date +%Y-%m-%d-%H:%M:%S)
-name_prefix='JMLREXPS_negaux_eubo_grid'
+name_prefix='FIN_negaux_eubo_grid'
 color=True
 train_dataset='cifar10'
 test_dataset_a='cifar10'
@@ -57,12 +57,12 @@ eubo_lambda=0.0
 z_num_samples=1
 seed=0
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=0
 
 tags="${name_prefix},${neg_dataset},${test_dataset_a},${test_dataset_b},${obs_noise_model}"
 name="${d}_${name_prefix}_${test_dataset_a}_vs_${test_dataset_b}_${obs_noise_model}_quantized=${add_obs_noise}_neg_dataset=${neg_dataset}_seed=${seed}"
 echo $name
-for eubo_neg_labda in 0.001 0.01 0.1 1.0 10.0 100.0 1000.0
+for eubo_neg_lambda in 0.001 0.01 0.1 1.0 10.0 100.0 1000.0
 do
     CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} python main.py --z_num_samples ${z_num_samples} --eubo_neg_lambda ${eubo_neg_lambda} --eubo_lambda ${eubo_lambda} --encoder_use_sn ${encoder_use_sn} --beta_neg ${beta_neg} --generator_adversarial_loss ${generator_adversarial_loss} --tags ${tags} --neg_prior ${neg_prior} --neg_prior_mean_coeff ${neg_prior_mean_coeff} --initial_log_gamma ${initial_log_gamma} --trained_gamma ${trained_gamma} --optimizer ${optimizer} --add_obs_noise ${add_obs_noise} --reg_lambda ${reg_lambda} --obs_noise_model ${obs_noise_model} --neg_train_size ${neg_train_size} --neg_test_size ${neg_test_size} --alpha_neg ${alpha_neg} --neg_dataset ${neg_dataset} --name "${name}" --seed ${seed} --color ${color} --use_augmented_variance_loss ${use_augmented_variance_loss} --test_dataset_b ${test_dataset_b} --test_dataset_a ${test_dataset_a} --model_architecture ${model_architecture} --oneclass_eval ${oneclass_eval} --normal_class ${normal_class} --gcnorm ${gcnorm} --dataset ${train_dataset} --shape ${shape} --train_size ${train_size} --test_size ${test_size} --m ${m} --alpha_reconstructed ${alpha_reconstructed} --alpha_generated ${alpha_generated} --beta ${beta} --latent_dim ${latent_dim} --batch_size ${batch_size} --lr ${lr} --nb_epoch ${nb_epoch} --prefix ${save_dir}/${name} --save_latent ${save_latent} --base_filter_num ${base_filter_num} --encoder_use_bn ${encoder_use_bn} --encoder_wd ${encoder_wd} --generator_use_bn ${generator_use_bn} --generator_wd ${generator_wd} --frequency ${frequency} --verbose ${verbose} --resnet_wideness ${resnet_wideness} --lr_schedule ${lr_schedule} > ${save_dir}/${name}.cout 2> ${save_dir}/${name}.cerr
 done
